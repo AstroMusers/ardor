c2daf5cad6ce4b7d3297f718243fba8397b75833
Nathan Whitsett <whitsett.n@astromusers.wustl.edu>
2025-10-30 15:59:08 -0500

    Modified flare_energy() to now dynamically accept multi-peaked flares

--- src/ardor/Flares/allesfitter_priors.py
+++ src/ardor/Flares/allesfitter_priors.py
@@ -13,22 +13,14 @@ import os
 from ardor.Flares import aflare
 import math
 from ardor.Utils.planck_law import planck_law, planck_integrator
+from ardor.Utils.Utils import asymmetric_sample
+import allesfitter
 
 def find_nearest(array, value):
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return array[idx], idx
-def flare_settings(data_file_dir, settings_template_dir, output_dir, flares=1, multi_process = False, cores = 1):
-    settings = pd.read_csv(settings_template_dir, index_col=False)
-    name = (data_file_dir.rsplit('/', 1)[1])[:-4]
-    settings.at[5, 'value'] = name
-    settings.at[10, 'value'] = multi_process
-    if multi_process == True:
-        settings.at[11, 'value'] = cores
-    settings.at[33, '#name'] = 'baseline_flux_' + str(name)
-    settings.at[37, '#name'] = 'error_flux_' + str(name)
-    settings.at[41, 'value'] = flares
-    settings.to_csv(output_dir, index=False)
+
 def exp_decay(x, a, b, c):
     '''
     
@@ -51,131 +43,65 @@ def exp_decay(x, a, b, c):
 
     '''
     return a * np.exp(-b * x) + c
-def allesfitter_priors(flare_csv, time_unit='min'):
-    flare = pd.read_csv(flare_csv, header=None)
-    flare.reset_index(inplace=True, drop=True)
-    time = flare[0]
-    flux = flare[1] - 1.0
-    time_factor = 1140
-    # try:
-    if time_unit == 'min':
-        params, pcov = curve_fit(exp_decay, time[int(np.where(time==0)[0]): int(np.where(time==0)[0]) + 30], flux[int(np.where(time==0)[0]): int(np.where(time==0)[0]) + 30], maxfev=5000)
-        time_factor = 1140
-    elif time_unit == 'days':
-        params, pcov = curve_fit(exp_decay, flux[np.where(time==0)[0]: np.where(time==0)[0] + 30]*1140, flux[np.where(time==0)[0]: np.where(time==0)[0] + 30], maxfev=5000)
-        time_factor = 1
-    # except:
-    #     params = (0, 5.0, 0.01)
-    #     time_factor = 1140
-    x = np.linspace(0, 10, num = 1500)/(1140)
-    y = exp_decay(x, params[0], params[1]*int(time_factor), params[2])
-    amp, idx = find_nearest(y, y.max()/2)
-    amp = flux.max()
-    tau = x[idx]
-    for index in range(len(flare[2])):
-        if math.isnan(flare[2][index]) == True:
-            flare[2][index] = 1e-3
-    if np.abs(flare[0][0]) > 1:
-        flare[0] = flare[0]/time_factor
-    flare.to_csv(flare_csv, index=False, header=False)
-    return amp, tau
 
-def flare_params(data_file_dir, params_template_dir, output_dir, flares=1):
-    amp, tau = allesfitter_priors(data_file_dir)
-    data = pd.read_csv(data_file_dir, index_col = False, header=None)
-    error = data[2]
-    for index in range(len(error)):
-        if math.isnan(error[index]) == True:
-            error[index] = 1e-3
-    flux_err = np.log(np.median(error))
-    params = pd.read_csv(params_template_dir, index_col=False)
-    name = (data_file_dir.rsplit('/', 1)[1])[:-4]
-    params.at[1, 'value'] = 0
-    params.at[2, 'value'] = tau
-    params.at[3, 'value'] = amp
-    params.at[5, 'value'] = flux_err
-    params.at[1, 'bounds'] = 'uniform ' + str(-0.001) + ' ' + str(0.001)
-    params.at[2, 'bounds'] = 'uniform ' + str(0) + ' ' + str(2.5*tau)
-    params.at[3, 'bounds'] = 'uniform ' + str(amp) + ' ' + str(3.5*amp)
-    params.at[5, 'bounds'] = 'uniform ' + str(-1 + flux_err) + ' ' + str(1 + flux_err)
-    # if flares > 1:
-    #     for number in range(2, flares+1):
-    #         params.at[1+3*(number-1), '#name'] = 'flare_tpeak_' + str(number)
-    #         params.at[2+3*(number-1), '#name'] = 'flare_fwhm_' + str(number)
-    #         params.at[3+3*(number-1), '#name'] = 'flare_ampl_' + str(number)
-            
-    #     params.at[5+3*(flares-1), '#name'] = '#errors (overall scaling) per instrument'
-    #     params.at[6+3*(flares-1), '#name'] = 'log_err_flux_' + str(name)
-    #     params.at[6+3*(flares-1), 'label'] = '$\log{\sigma_\mathrm{' + str(name) + '}}$'
-    # else:
-    params.at[5, '#name'] = '#errors (overall scaling) per instrument'
-    params.at[5, '#name'] = 'ln_err_flux_' + str(name)
-    params.at[5, 'label'] = '$\ln{\sigma_\mathrm{' + str(name) + '}}$'
-    params.to_csv(output_dir, index=False)
-
-def return_parameters(mcmc_table_dir):
-    data = pd.read_csv(mcmc_table_dir)
-    t_peak = data['median'][1]
-    t_peak_m = data['lower_error'][1]
-    t_peak_p = data['upper_error'][1]
-    fwhm = data['median'][2]
-    fwhm_m = data['lower_error'][2]
-    fwhm_p = data['upper_error'][2]
-    amp = data['median'][3]
-    amp_m = data['lower_error'][3]
-    amp_p = data['upper_error'][3]
-
-    return [t_peak, t_peak_m, t_peak_p, fwhm, fwhm_m, fwhm_p, amp, amp_m, amp_p]
-
-def flare_energy(fwhm, ampl, Teff, R_stellar, uncertainty = False, fwhm_u = 0, fwhm_l = 0, ampl_u = 0, ampl_l = 0,
-                 R_stellar_l = 0, R_stellar_u = 0):
-    if uncertainty == False:
-        x = np.linspace(0, 0.02, num = 2000)
-        y = aflare.aflare1(x, 0.01, fwhm, ampl)
-        flare_area = simpson(y, x)
-        color_factor = planck_integrator(600e-6, 1000e-6, Teff)/planck_integrator(600e-6, 1000e-6, 9000)
-        energy = (5.67e-8)*(9000**4)*(flare_area)*np.pi*(R_stellar*6.957e8*R_stellar*6.957e8)*color_factor*(1e7)*86400
-        return energy
-    if uncertainty == True:
-        x = np.linspace(0, 0.02, num = 2000)
-        y = aflare.aflare1(x, 0.01, fwhm, ampl)
-        flare_area = simpson(y, x)
-        color_factor = planck_integrator(600e-6, 1000e-6, Teff)/planck_integrator(600e-6, 1000e-6, 9000)
-        energy = (5.67e-8)*(9000**4)*(flare_area)*np.pi*(R_stellar*6.957e8*R_stellar*6.957e8)*color_factor*(1e7)*86400
-        samples = []
-        for flare_samples in range(1000):
-            fwhm_sample = fwhm
-            ampl_sample = ampl
-            R_stellar_sample = R_stellar
-            
-            check1 = np.random.random()
-            check2 = np.random.random()
-            check3 = np.random.random()
-            if check1 < 0.5:
-                err = np.abs(np.random.normal(0, scale = fwhm_l))
-                fwhm_sample -= err
-            elif check1 > 0.5:
-                err = np.abs(np.random.normal(0, scale = fwhm_u))
-                fwhm_sample += err
-            if check2 < 0.5:
-                err = np.abs(np.random.normal(0, scale = ampl_l))
-                ampl_sample -= err
-            elif check2 > 0.5:
-                err = np.abs(np.random.normal(0, scale = fwhm_u))
-                ampl_sample += err
-            if check3 < 0.5:
-                err = np.abs(np.random.normal(0, scale = R_stellar_l))
-                R_stellar_sample -= err
-            elif check3 > 0.5:
-                err = np.abs(np.random.normal(0, scale = R_stellar_u))
-                R_stellar_sample += err
-            
-            y = aflare.aflare1(x, 0.01, fwhm_sample, ampl_sample)
-            flare_temp = np.random.normal(loc=9000, scale=500)
-            color_factor = planck_integrator(600e-6, 1000e-6, Teff)/planck_integrator(600e-6, 1000e-6, flare_temp)
-            energy_sample = (5.67e-8)*(9000**4)*(flare_area)*np.pi*(R_stellar*6.957e8*R_stellar*6.957e8)*color_factor*(1e7)*86400
-            samples.append(energy_sample)
-        return energy, samples[320], samples[680]
+def flare_energy(params, Teff, R_stellar, uncertainty = False, param_lower = [0,0,0], param_upper = [0,0,0], Teff_l = 0, Teff_u = 0,
+                 R_stellar_l = 0, R_stellar_u = 0, N = 1, N_samp = 1000):
+    if len(params) != 3*N:
+        raise ValueError("Parameter length does not match number of flares.")
+    for flares in range(N):
+        if uncertainty == False:
+            x = np.linspace(0, 0.02, num = 2000)
+            y = aflare.aflare(x, params)
+            flare_area = simpson(y, x)
+            color_factor = planck_integrator(600e-6, 1000e-6, Teff)/planck_integrator(600e-6, 1000e-6, 9000)
+            energy = (5.67e-8)*(9000**4)*(flare_area)*np.pi*(R_stellar*6.957e8*R_stellar*6.957e8)*color_factor*(1e7)*86400
+            return energy
+        if uncertainty == True:
+            x = np.linspace(0, 0.02, num = 2000)
+            y = aflare.aflare(x, params)
+            flare_area = simpson(y, x)
+            color_factor = planck_integrator(600e-6, 1000e-6, Teff)/planck_integrator(600e-6, 1000e-6, 9000)
+            energy = (5.67e-8)*(9000**4)*(flare_area)*np.pi*(R_stellar*6.957e8*R_stellar*6.957e8)*color_factor*(1e7)*86400
+            samples = []
+            for flare_samples in range(N_samp):
+                if N == 1:
+                    R_stellar_sample = asymmetric_sample(R_stellar, R_stellar_u, R_stellar_l)
+                    Teff_sample = asymmetric_sample(Teff, Teff_u, Teff_l)
+                    fwhm_sample = asymmetric_sample(params[1], param_upper[1], param_lower[1])
+                    ampl_sample = asymmetric_sample(params[2], param_upper[2], param_lower[2])
+                    y = aflare.aflare(x, [params[0], fwhm_sample, ampl_sample])
+                    flare_temp = np.random.normal(loc=9000, scale=500)
+                    color_factor = planck_integrator(600e-6, 1000e-6, Teff_sample)/planck_integrator(600e-6, 1000e-6, flare_temp)
+                    energy_sample = (5.67e-8)*(flare_temp**4)*(flare_area)*np.pi*(R_stellar_sample*6.957e8*R_stellar_sample*6.957e8)*color_factor*(1e7)*86400
+                    samples.append(energy_sample)
+                if N == 2:
+                    R_stellar_sample = asymmetric_sample(R_stellar, R_stellar_u, R_stellar_l)
+                    Teff_sample = asymmetric_sample(Teff, Teff_u, Teff_l)
+                    fwhm1_sample = asymmetric_sample(params[1], param_upper[1], param_lower[1])
+                    ampl1_sample = asymmetric_sample(params[2], param_upper[2], param_lower[2])
+                    fwhm2_sample = asymmetric_sample(params[4], param_upper[4], param_lower[4])
+                    ampl2_sample = asymmetric_sample(params[5], param_upper[5], param_lower[5])
+                    y = aflare.aflare(x, [params[0], fwhm1_sample, ampl1_sample, params[3], fwhm2_sample, ampl2_sample])
+                    flare_temp = np.random.normal(loc=9000, scale=500)
+                    color_factor = planck_integrator(600e-6, 1000e-6, Teff_sample)/planck_integrator(600e-6, 1000e-6, flare_temp)
+                    energy_sample = (5.67e-8)*(flare_temp**4)*(flare_area)*np.pi*(R_stellar_sample*6.957e8*R_stellar_sample*6.957e8)*color_factor*(1e7)*86400
+                    samples.append(energy_sample)
+                if N == 3:
+                    R_stellar_sample = asymmetric_sample(R_stellar, R_stellar_u, R_stellar_l)
+                    Teff_sample = asymmetric_sample(Teff, Teff_u, Teff_l)
+                    fwhm1_sample = asymmetric_sample(params[1], param_upper[1], param_lower[1])
+                    ampl1_sample = asymmetric_sample(params[2], param_upper[2], param_lower[2])
+                    fwhm2_sample = asymmetric_sample(params[4], param_upper[4], param_lower[4])
+                    ampl2_sample = asymmetric_sample(params[5], param_upper[5], param_lower[5])
+                    fwhm3_sample = asymmetric_sample(params[7], param_upper[7], param_lower[7])
+                    ampl3_sample = asymmetric_sample(params[8], param_upper[8], param_lower[8])
+                    y = aflare.aflare(x, [params[0], fwhm1_sample, ampl1_sample, params[3], fwhm2_sample, ampl2_sample, params[6], fwhm3_sample, ampl3_sample])
+                    flare_temp = np.random.normal(loc=9000, scale=500)
+                    color_factor = planck_integrator(600e-6, 1000e-6, Teff_sample)/planck_integrator(600e-6, 1000e-6, flare_temp)
+                    energy_sample = (5.67e-8)*(flare_temp**4)*(flare_area)*np.pi*(R_stellar_sample*6.957e8*R_stellar_sample*6.957e8)*color_factor*(1e7)*86400
+                    samples.append(energy_sample)
+            samples = sorted(samples)
+            return energy, -np.abs(energy - samples[int(N_samp*0.32)]), np.abs(energy - samples[int(N_samp*0.68)])
 
 def csv_cleaner(flare_csv_dir):
     data = pd.read_csv(flare_csv_dir, header=None, index_col=False)
@@ -212,15 +138,7 @@ def run_ns(target_file, working_dir, param_template_dir, settings_template_dir, 
     #Check each folder
     # Copy relevant file to allesfitter folder
     name = os.path.basename(os.path.normpath(target_file))
-    shutil.copyfile(target_file, working_dir + '/' + name)
     csv_cleaner(working_dir + '/' + name)
-    # Run allesfitter
-    if baseline == False:
-        flare_params(working_dir + '/' + name, param_template_dir, working_dir + '/params.csv')
-        flare_settings(working_dir + '/' + name, settings_template_dir, working_dir + '/settings.csv', multi_process=True, cores=63, flares = 1)
-    elif baseline == True:
-        flare_params(working_dir + '/' + name, param_template_dir, working_dir + '/params.csv')
-        flare_settings(working_dir + '/' + name, settings_template_dir, working_dir + '/settings.csv', multi_process=True, cores=63, flares=0)
     allesfitter.ns_fit(working_dir)
     allesfitter.ns_output(working_dir)
     os.remove(working_dir + '/' + name)
